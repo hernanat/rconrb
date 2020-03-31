d92f362b94ff73cc362a732867791e6f28c88022
BuildTools <ant@antfeedr.com>
2020-03-31 15:40:19 -0700

    updates gemspec with documentation info
    Also fixes an error in documentation

--- lib/rcon.rb
+++ lib/rcon.rb
@@ -2,7 +2,7 @@ require "rcon/version"
 require "rcon/error/error"
 require "rcon/client"
 
-# This module is based on the protocol{https://developer.valvesoftware.com/wiki/Source_RCON_Protocol Source RCON Protocol}
+# This module is based on the {https://developer.valvesoftware.com/wiki/Source_RCON_Protocol Source RCON Protocol}.
 # It is to be used for executing remote commands on servers that implement this protocol, or various flavors of it.
 #
 # The goal was to design something that could be used to work with the default protocol implementation, but also offer
